8306dbfbc9b2d60eb6bfd70a68d5598f1860e3ea
djenidisimple <djenidigauss@gmail.com>
2025-12-22 21:26:25 +0300

    i add new function who fixe min bug

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let div, body = document.body, span, textUser = [], color = "";
 let content = document.querySelector(".content");
 let cursor = (textUser.length - 1 < 0) ? 0 : textUser.length - 1;
 let restart = document.querySelector(".btn-restart");
+let main = document.querySelector("main"), config = document.querySelector(".config");
 background.className = "background";
 body.appendChild(background);
 easy.split("").forEach(value => {
@@ -13,7 +14,7 @@ easy.split("").forEach(value => {
 
 span = document.querySelectorAll("span.text");
 
-
+// background
 for (let i = 0; i < 12; i++) {
     div = document.createElement("div");
     div.className = "items";
@@ -22,11 +23,16 @@ for (let i = 0; i < 12; i++) {
 
 document.addEventListener("keydown", function(e) {
     let regex = /[a-zA-Z0-9@.,/?&!#$%^&*()=-`~'";<>\\|\[\]{}\e]/;    
-    if (e.key.length === 1 && (regex.test(e.key) || e.keyCode == 32)) {
+    if (e.key.length === 1 && (regex.test(e.key) || e.keyCode == 32) && cursor >= 0) {
         textUser.push(e.key);
         color = (e.key == easy.split("")[cursor]) ? "var(--green-500)" : "var(--red-500)";
         span[cursor].style.color = color;
-        cursor++;
+        if (cursor + 1 == easy.split("").length) {
+            cursor = 0;
+            main.style.display = "none";
+        } else {
+            cursor++;
+        }
     } else if (e.key == "Backspace" || e.key == "Delete" && textUser.length > 0) {
         span[cursor - 1].style.color = "var(--neutral-400)";
         textUser.pop()
@@ -34,8 +40,10 @@ document.addEventListener("keydown", function(e) {
     }
 });
 
+
 restart.addEventListener("click", function() {
     textUser = [];
     cursor = 0;
     span.forEach(value => {value.style.color = "var(--neutral-400)"});
+    main.style.display = "block";
 });
